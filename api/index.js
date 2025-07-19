5bdfff7b6cec624aa2bb7e7d422cda5e3a7bd955
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2025-07-19 21:56:55 +0600

    cors config

--- api/index.js
+++ api/index.js
@@ -15,7 +15,22 @@ const messageRoutes = require('../routes/messageRoutes');
 const userRoutes = require('../routes/userRoutes');
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+    'https://fahmidurshanto.vercel.app',
+    'http://localhost:5173'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 app.use(express.json());
 
 // MongoDB Connection
